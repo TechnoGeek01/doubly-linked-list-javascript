092d6634afc5a0a56977c5eaebcbfa09900acb02
TechnoGeek01 <rsudharsangames@gmail.com>
2023-08-15 13:59:05 +0530

    added get, set, insert, remove methods

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -78,6 +78,81 @@ class DoublyLinkedList {
 
     this.length++;
 
-    return newNode;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+
+    var current, count;
+
+    if (index <= this.length / 2) {
+      currNode = this.head;
+      count = 0;
+
+      while (count != index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      currNode = this.tail;
+      count = this.length - 1;
+
+      while (count != index) {
+        current = current.prev;
+        count--;
+      }
+    }
+
+    return current;
+  }
+
+  set(index, val) {
+    var setNode = this.get(index);
+    if (setNode) {
+      setNode.val = val;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return undefined;
+    if (index === 0) return !!this.unshift();
+    if (index === this.length) return !!this.push();
+
+    var newNode = new Node(val);
+
+    var prevNode = this.get(index - 1);
+    var nextNode = prevNode.next;
+
+    prevNode.next = newNode;
+    nextNode.prev = newNode;
+
+    newNode.prev = prevNode;
+    newNode.next = nextNode;
+
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return !!this.shift();
+    if (index === this.length - 1) return !!this.pop();
+
+    var removedNode = this.get(index);
+    var prevNode = removedNode.prev;
+    var nextNode = removedNode.next;
+
+    prevNode.next = nextNode;
+    nextNode.prev = prevNode;
+
+    removedNode.next = null;
+    removedNode.prev = null;
+
+    this.length--;
+
+    return removedNode;
   }
 }
